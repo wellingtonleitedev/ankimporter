699c6de9d196d7c7ff43eba740b22c441ca15212
Wellington Leite <wellingtonleitee.s@gmail.com>
2023-08-27 18:54:25 -0300

    [Closes #8] redesign the app layout

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -5,17 +5,41 @@ const Home = () => {
   const { data, fetchData, onSubmit } = useHome();
 
   return (
-    <main className="m-auto max-w-5xl px-0.5 py-5">
-      <h1 className="text-3xl text-center font-bold">Ankimporter</h1>
-      <form onSubmitCapture={onSubmit} className="flex justify-between my-10">
-        <input type="file" name="notes" onChange={fetchData} />
-        <input multiple type="file" name="audios" onChange={fetchData} />
-        <button type="submit" disabled={!data.length}>
-          Import
-        </button>
-      </form>
-      <Notes data={data} />
-    </main>
+    <>
+      <header className="bg-zinc-900 border-b">
+        <div className="m-auto max-w-5xl py-5">
+          <h1 className="text-3xl font-bold">Ankimporter</h1>
+        </div>
+      </header>
+      <main className="m-auto max-w-5xl py-5">
+        <form onSubmitCapture={onSubmit} className="flex justify-between my-10">
+          <input
+            type="file"
+            name="notes"
+            className="rounded"
+            onChange={fetchData}
+            accept="application/pdf"
+          />
+          <input
+            multiple
+            type="file"
+            name="audios"
+            accept="audio/*"
+            className="rounded"
+            onChange={fetchData}
+            placeholder="No audio chosen"
+          />
+          <button
+            className="bg-slate-50 text-zinc-800 px-2 py-1 rounded"
+            type="submit"
+            disabled={!data.length}
+          >
+            Import
+          </button>
+        </form>
+        <Notes data={data} />
+      </main>
+    </>
   );
 };
 
